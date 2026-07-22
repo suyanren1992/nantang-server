45673d0dc5778b7246c1829819fe47dae43af4a7
Nantang <nantang@example.com>
2026-07-22 14:42:17 +0800

    fix: CORS支持Pages域名 + 更新API地址

--- server/main.py
+++ server/main.py
@@ -63,6 +63,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://nantang-server.pages.dev",
+        "https://*.trycloudflare.com",
+        "https://*.pages.dev",
         os.environ.get("FRONTEND_ORIGIN", "http://localhost:8000"),
     ],
     allow_credentials=True,
